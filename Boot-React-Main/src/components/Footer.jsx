f2acdcd09aaa9c64680f57ca7e0653c8bda413e3
musabReyiz5643 <akgunmusab3@gmail.com>
2026-06-23 21:52:19 +0300

    Footer Design was changed and fixed

--- Boot-React-Main/src/components/Footer.jsx
+++ Boot-React-Main/src/components/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="attribution">
+    <footer className="attribution absolute bottom-0 lg:bottom-10 left-1/2 -translate-x-1/2 w-full ">
       Challenge by{" "}
       <a href="https://www.frontendmentor.io?ref=challenge">Frontend Mentor</a>.
       Coded by <a href="#">Musab Akgün</a>.
